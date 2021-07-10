804f5bd7dd3fa90594d9c748ed57629c60492635
furimu1234 <68190216+furimu1234@users.noreply.github.com>
2021-07-10 20:58:38 +0900

    二重ループの答え

--- answer.java
+++ answer.java
@@ -1,35 +1,13 @@
-import java.util.Random; // ランダムを扱うためのプラグインをこのプログラムに読み込む
+public class answer { // いつもの
+	public static void main(String[] args) { // いつもの
 
-public class answer {
+		int[][] numbers = {{1, 2}, {3, 4}, {5, 6}}; //numbers変数という配列を作り、その中に3つ配列を作る (2次元配列)
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		for (int num = 10; num >= -20; num--) { // numを10にセットして numが-20になるまで-1する
-			Random random = new Random(); //　おまじない
-			boolean result = random.nextBoolean(); // True or False のどちらかを変数 resultにプログラムが自動で代入する
-			if (num % 2 == 0 && result == true) { // num が2で割り切れ、かつ、resultがTrueの場合
-				System.out.println(num + "　÷ 2 = 0 です。 (true = 1)"); 
+		for (int one=0; numbers.length > one; one++) {
+			for (int two=0; numbers[one].length  > two; two++) {
+
+				System.out.println("番号: " + numbers[one][two]);
 			}
-			
-			else if (num % 2 == 0 && result == false) { // num が2で割り切れ、かつ、resultがfalseの場合
-				System.out.println(num + "　÷ 2 = 0 だと思います。 (false = 0)");
-			}
-			
-			else if (num % 5 == 0 && result == true) { // num が5で割り切れ、かつ、resultがtrueの場合
-				System.out.println(num + "　÷ 5 = 0 です。 (true = 1)");
-			}
-			
-			else if (num % 5 == 0 && result == false) { // num が5で割り切れ、かつ、resultがfalseの場合
-				System.out.println(num + "　÷ 5 = 0 だと思います。 (false = 0)");
-			}
-			
-			else { // 上記のどれにも当てはまらない場合
-				System.out.println(num + "　は未知の数値です。");
-			}
-			
-			// 条件関係なしで出力する
-			System.out.println("--------------------------------------");
 		}
 
 	}
