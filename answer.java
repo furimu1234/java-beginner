07b09b377b09b79707eccfb62439fdbce75b611b
furimu1234 <68190216+furimu1234@users.noreply.github.com>
2021-07-18 15:46:19 +0900

    for!

--- answer.java
+++ answer.java
@@ -1,17 +1,29 @@
 public class answer { // いつもの
 	public static void main(String[] args) { // いつもの
+		int[] tensu = {80, 63, 70, 45, 90, 100};
 
-		int[][] numbers = {{1, 2}, {3, 4}, {5, 6}}; //numbers変数という配列を作り、その中に3つ配列を作る (2次元配列)
-		int pair = 1;
-
-		for (int one=0; numbers.length > one; one++) {
-			for (int two=0; numbers[one].length  > two; two++) {
-
-				System.out.println(pair + "人目: " + numbers[one][two]);
-				
-				pair ++;
-			}
+		int a, i;
+		for (int n: tensu) {
+			a=n/20;
+			System.out.println(a);
 		}
+
+		System.out.println("----------------");
+
+		for (i=0; i<tensu.length; i++){
+			a = tensu[i]/20;
+			System.out.println(a);
+		}
+		System.out.println("----------------");
+
+		for (i=0; i<tensu.length; i++){
+			int n=tensu[i];
+			a=n/20;
+		}
+		System.out.println("----------------");
+
+
+	
 
 	}
 
